e25d533d604f18769ac6ea38431e1ebef57fa638
Limingyue <113318496+hellomingyueQQ@users.noreply.github.com>
2024-09-03 21:57:33 +0800

    Update edit-contact.component.ts

--- src/app/edit-contact/edit-contact.component.ts
+++ src/app/edit-contact/edit-contact.component.ts
@@ -1,24 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { ContactsService } from '../contacts/contacts.service';
 
 @Component({
   templateUrl: './edit-contact.component.html',
   styleUrls: ['./edit-contact.component.css'],
 })
 export class EditContactComponent implements OnInit {
-  firstName = new FormControl(); // collecting element value and perform validation
+  // 1. form control给form element传递值，一般给intial value
+  firstName = new FormControl('Jim');
   lastName = new FormControl();
   dateOfBirth = new FormControl();
   favoritesRanking = new FormControl();
-  // next step: wire up the form controls to the input
-  // then angular will track input state, like value, if the filed is dirty or touched, valid
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private contactService: ContactsService
+  ) {}
 
   ngOnInit() {
+    // 2. create form control之后，再给form control赋值
+    // 使用setValue
     const contactId = this.route.snapshot.params['id'];
     if (!contactId) return;
+    this.contactService.getContact(contactId).subscribe((contact) => {
+      if (!contact) {
+        return;
+      }
+      this.firstName.setValue(contact.firstName);
+      this.lastName.setValue(contact.lastName);
+      this.dateOfBirth.setValue(contact.dateOfBirth);
+      this.favoritesRanking.setValue(contact.favoritesRanking);
+    });
   }
 
   saveContact() {
