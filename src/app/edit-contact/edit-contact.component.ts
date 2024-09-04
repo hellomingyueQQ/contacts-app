d116b5723c65bfc9727bc0960a5ced7072240bc0
Limingyue <113318496+hellomingyueQQ@users.noreply.github.com>
2024-09-04 22:52:31 +0800

    Update edit-contact.component.ts

--- src/app/edit-contact/edit-contact.component.ts
+++ src/app/edit-contact/edit-contact.component.ts
@@ -1,29 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ContactsService } from '../contacts/contacts.service';
+import { first } from 'rxjs';
 
 @Component({
   templateUrl: './edit-contact.component.html',
   styleUrls: ['./edit-contact.component.css'],
 })
 export class EditContactComponent implements OnInit {
-  contactForm = new FormGroup({
-    id: new FormControl(), // not bind to html template, but the id will be shown in this.contactForm.value
-    firstName: new FormControl(), // Question: if FormControl is not initialized, then the type of firstName is any, is this okay?
-    lastName: new FormControl(),
-    dateOfBirth: new FormControl(),
-    favoritesRanking: new FormControl(),
-    phone: new FormGroup({
-      phoneNumber: new FormControl(),
-      phoneType: new FormControl(),
+  // Question: if FormControl is not initialized, then the type of firstName is any, is this okay?
+  // 类型最好匹配绑定数据的类型
+  // 1. 使用generic 的类型，同时要给初始化值  new FormControl<string>(""),
+  // 但是给了初始化值，不需要给generic type  new FormControl("")，但是id这个时候的类型为string或者null
+  // Question: 这不符合Contact的id的类型，怎么办?
+  // string|null=>string  new FormControl('',{nonNullable:true}),
+
+  // 下面的group是nullable的写法，如果是not nullable的写法：this.fb.nonNullable.group
+  contactForm = this.fb.nonNullable.group({
+    id: '',
+    firstName: '',
+    lastName: '',
+    dateOfBirth: <Date | null>null, //如果仅为null，那么类型为null
+    favoritesRanking: <number | null>null, //
+    phone: this.fb.nonNullable.group({
+      phoneNumber: '',
+      phoneType: '',
     }),
-    address: new FormGroup({
-      streetAddress: new FormControl(),
-      city: new FormControl(),
-      state: new FormControl(),
-      postalCode: new FormControl(),
-      addressType: new FormControl(),
+    address: this.fb.nonNullable.group({
+      streetAddress: '',
+      city: '',
+      state: '',
+      postalCode: '',
+      addressType: '',
     }),
   });
   // 这里的问题是defining formgroup messy
@@ -31,8 +40,10 @@ export class EditContactComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private contactService: ContactsService,
-    private router: Router
+    private router: Router,
+    private fb: FormBuilder
   ) {}
+  // 使用FormBulder可以简化formgroup的定义， 它是inject来的
 
   ngOnInit() {
     const contactId = this.route.snapshot.params['id'];
@@ -41,35 +52,55 @@ export class EditContactComponent implements OnInit {
       if (!contact) {
         return;
       }
-      this.contactForm.controls.id.setValue(contact.id);
-      this.contactForm.controls.firstName.setValue(contact.firstName);
-      this.contactForm.controls.lastName.setValue(contact.lastName);
-      this.contactForm.controls.dateOfBirth.setValue(contact.dateOfBirth);
-      this.contactForm.controls.favoritesRanking.setValue(
-        contact.favoritesRanking
-      );
-      this.contactForm.controls.phone.controls.phoneNumber.setValue(
-        contact.phone.phoneNumber
-      );
-      this.contactForm.controls.phone.controls.phoneType.setValue(
-        contact.phone.phoneType
-      );
-      this.contactForm.controls.address.controls.streetAddress.setValue(
-        contact.address.streetAddress
-      );
-      this.contactForm.controls.address.controls.city.setValue(
-        contact.address.city
-      );
-      this.contactForm.controls.address.controls.postalCode.setValue(
-        contact.address.postalCode
-      );
-      this.contactForm.controls.address.controls.state.setValue(
-        contact.address.state
-      );
-      this.contactForm.controls.address.controls.addressType.setValue(
-        contact.address.addressType
-      );
+      // setValue不设置generic那就是any数据类型可以设置进去
+      // this.contactForm.controls.id.setValue(contact.id);
+      // this.contactForm.controls.firstName.setValue(contact.firstName);
+      // this.contactForm.controls.lastName.setValue(contact.lastName);
+      // this.contactForm.controls.dateOfBirth.setValue(contact.dateOfBirth);
+      // this.contactForm.controls.favoritesRanking.setValue(
+      //   contact.favoritesRanking
+      // );
+      // this.contactForm.controls.phone.controls.phoneNumber.setValue(
+      //   contact.phone.phoneNumber
+      // );
+      // this.contactForm.controls.phone.controls.phoneType.setValue(
+      //   contact.phone.phoneType
+      // );
+      // this.contactForm.controls.address.controls.streetAddress.setValue(
+      //   contact.address.streetAddress
+      // );
+      // this.contactForm.controls.address.controls.city.setValue(
+      //   contact.address.city
+      // );
+      // this.contactForm.controls.address.controls.postalCode.setValue(
+      //   contact.address.postalCode
+      // );
+      // this.contactForm.controls.address.controls.state.setValue(
+      //   contact.address.state
+      // );
+      // this.contactForm.controls.address.controls.addressType.setValue(
+      //   contact.address.addressType
+      // );
       // 这里的问题是初始化formgroup messy
+      this.contactForm.setValue(contact);
+      //这个代码可以拆分，可以简单些，比如address和phone再调用controls.xxx.setValue
+      // this.contactForm.controls.id.setValue(contact.id);
+      // this.contactForm.controls.firstName.setValue(contact.firstName);
+      // this.contactForm.controls.lastName.setValue(contact.lastName);
+      // this.contactForm.controls.dateOfBirth.setValue(contact.dateOfBirth);
+      // this.contactForm.controls.favoritesRanking.setValue(
+      //   contact.favoritesRanking
+      // );
+      // this.contactForm.controls.phone.setValue(contact.phone);
+      // this.contactForm.controls.address.setValue(contact.address);
+
+      //部分修改，比如，我想修改firstName,lastName不用control的setvalue，使用patchValue
+      const names = {
+        firstName: contact.firstName,
+        lastName: contact.lastName,
+      };
+      this.contactForm.patchValue(names);
+      // patchValue只能修改部分值，不能修改整个formgroup的值
     });
   }
 
