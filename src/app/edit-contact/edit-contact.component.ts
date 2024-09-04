a5d8bae0353b07122d119351f5c0d4493c6a9c2d
Nancy Li <NancyLi@Eurofins.com>
2024-09-04 11:38:31 +0800

    update

--- src/app/edit-contact/edit-contact.component.ts
+++ src/app/edit-contact/edit-contact.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ContactsService } from '../contacts/contacts.service';
 
 @Component({
@@ -8,44 +8,34 @@ import { ContactsService } from '../contacts/contacts.service';
   styleUrls: ['./edit-contact.component.css'],
 })
 export class EditContactComponent implements OnInit {
-  // 这些form control是isolated，如果做form层级validation，需要formgroup
-  // formGroup更容易设置值，也更容易获取所有Form control的值
-  // 用formGroup可以实现form级别的validation，后面会讲
   contactForm = new FormGroup({
-    firstName: new FormControl('Jim'),
+    id: new FormControl(), // not bind to html template, but the id will be shown in this.contactForm.value
+    firstName: new FormControl(), // if FormControl is not initialized, then the type of firstName is any
     lastName: new FormControl(),
     dateOfBirth: new FormControl(),
     favoritesRanking: new FormControl(),
   });
 
-  constructor(
-    private route: ActivatedRoute,
-    private contactService: ContactsService
-  ) {}
+  constructor(private route: ActivatedRoute, private contactService: ContactsService, private router: Router) {}
 
   ngOnInit() {
     const contactId = this.route.snapshot.params['id'];
     if (!contactId) return;
-    this.contactService.getContact(contactId).subscribe((contact) => {
+    this.contactService.getContact(contactId).subscribe(contact => {
       if (!contact) {
         return;
       }
-      // 从formgroup的controls里面获取formcontrol，然后设置值
+      this.contactForm.controls.id.setValue(contact.id);
       this.contactForm.controls.firstName.setValue(contact.firstName);
       this.contactForm.controls.lastName.setValue(contact.lastName);
       this.contactForm.controls.dateOfBirth.setValue(contact.dateOfBirth);
-      this.contactForm.controls.favoritesRanking.setValue(
-        contact.favoritesRanking
-      );
+      this.contactForm.controls.favoritesRanking.setValue(contact.favoritesRanking);
     });
   }
 
   saveContact() {
-    // 更加方便的获取值方法
-    console.log(this.contactForm.value);
-    // console.log(this.contactForm.controls.firstName.value);
-    // console.log(this.contactForm.controls.lastName.value);
-    // console.log(this.contactForm.controls.dateOfBirth.value);
-    // console.log(this.contactForm.controls.favoritesRanking.value);
+    this.contactService.saveContact(this.contactForm.value).subscribe({
+      next: () => this.router.navigate(['/contacts']),
+    });
   }
 }
