83f42eae4ff1a6b177cb2b76eca72a08b09dd84f
Nancy Li <NancyLi@Eurofins.com>
2024-09-06 15:14:54 +0800

    Update edit-contact.component.ts

--- src/app/edit-contact/edit-contact.component.ts
+++ src/app/edit-contact/edit-contact.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ContactsService } from '../contacts/contacts.service';
-import { first } from 'rxjs';
+import { distinctUntilChanged, first } from 'rxjs';
 import { addressTypeValues, phoneTypeValues } from '../contacts/contact.model';
 import { restrictedWords } from '../validators/restricted-words.validators';
 
@@ -54,14 +54,35 @@ export class EditContactComponent implements OnInit {
     });
   }
 
+  stringCompare(a: any, b: any) {
+    return JSON.stringify(a) === JSON.stringify(b);
+  }
   createPhoneGroup() {
     const phoneGroup = this.fb.nonNullable.group({
       phoneNumber: '',
       phoneType: '',
       preferred: false,
     });
+
+    // phoneGroup.valueChanges.subscribe 如果想watch any value of the phonegroup
+    // phoneGroup.controls.preferred.statusChanges validity change会激发这个流
     // formControl都有valuechanges, 这个定义的非常有针对性，不是更上一层
-    phoneGroup.controls.preferred.valueChanges.subscribe(value => {});
+    phoneGroup.controls.preferred.valueChanges
+      .pipe(distinctUntilChanged(this.stringCompare)) //打破循环
+      .subscribe(value => {
+        if (value) {
+          // 动态添加validator，如果为true，那么phone number需要添加required的validator
+          phoneGroup.controls.phoneNumber.addValidators([Validators.required]);
+        } else {
+          // 动态删除validator
+          phoneGroup.controls.phoneNumber.removeValidators([Validators.required]);
+          //但是光上面那句，uncheck prefer后，phonenumber还是有warning，这是因为
+          // angular没有 revaluate the validity of this control
+          // 没有操作在input元素上，所以没有机会trigger phone number的valuation
+        }
+        phoneGroup.controls.phoneNumber.updateValueAndValidity();
+        // 有可能造成循环，updateValueAndValidity导致valueChanges
+      });
 
     return phoneGroup;
   }
